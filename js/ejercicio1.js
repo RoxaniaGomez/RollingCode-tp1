03a33663dbaee5daa2a1d0cf2071b0505eb5db8b
roxania <groxania@gmail.com>
2023-02-27 12:23:26 -0300

    hice ejercicio 1 al 4

---
+++ js/ejercicio1.js
@@ -0,0 +1,6 @@
+/*
+Dificultad:  🟢
+1.- Escribe un programa de una sola línea que haga 
+que aparezca en la pantalla un alert que diga “un mensaje”.
+*/
+alert("Hola mundo");
---
+++ js/ejercicio2.js
@@ -0,0 +1,5 @@
+/*Dificultad:  🟢
+2.- Escribe un programa de una sola línea que escriba en 
+la pantalla un texto que diga «Hello World» (document.write).
+Dificultad:  🟢*/
+document.write("Hello World");
